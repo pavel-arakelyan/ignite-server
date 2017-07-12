e67cb73376085590303efa163a75b37acadaf78b
Pavel Arakelyan <pavel.arakelyan@arm.synisys.com>
2017-07-12 19:36:10 +0400

    Ignite server

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,7 +1,5 @@
-import com.hazelcast.config.Config;
-import com.hazelcast.config.UserCodeDeploymentConfig;
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.IgniteConfiguration;
 
 /**
  * @author pavel.arakelyan on 7/10/17.
@@ -9,19 +7,10 @@ import com.hazelcast.core.HazelcastInstance;
 public class Starter {
 
 	public static void main(String[] args) {
-//		IgniteConfiguration cfg = new IgniteConfiguration();
-//		cfg.setPeerClassLoadingEnabled(true);
-////		cfg.setClassLoader(Starter.class.getClassLoader());
-//		cfg.setClientMode(false);
-//		Ignition.start(cfg);
-		Config config = new Config();
-		config.setClassLoader(Thread.currentThread().getContextClassLoader());
-		UserCodeDeploymentConfig distCLConfig = config.getUserCodeDeploymentConfig();
-		distCLConfig.setEnabled( true )
-				.setClassCacheMode( UserCodeDeploymentConfig.ClassCacheMode.ETERNAL )
-				.setProviderMode( UserCodeDeploymentConfig.ProviderMode.LOCAL_AND_CACHED_CLASSES );
-//				.setBlacklistedPrefixes( "com.foo" )
-//				.setProviderFilter( "HAS_ATTRIBUTE:lite" );
-		HazelcastInstance hzInstance = Hazelcast.newHazelcastInstance(config);
+		IgniteConfiguration cfg = new IgniteConfiguration();
+		cfg.setPeerClassLoadingEnabled(true);
+//		cfg.setClassLoader(Starter.class.getClassLoader());
+		cfg.setClientMode(false);
+		Ignition.start(cfg);
 	}
 }
